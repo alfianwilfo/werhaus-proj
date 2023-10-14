826d6bd8b5e95371170567725db8b75f3b5223e6
Alfian Horis <alfian.horis@merahciptamedia.co.id>
2023-10-14 17:08:44 +0700

    add tailwind

---
+++ werhaus-fe/src/views/user/Login.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: 'LoginPage',
+}
+</script>
+
+<template>
+    <div class="">
+        <h1 class="text-red-600">
+          Hello world!
+        </h1>
+    </div>
+</template>
